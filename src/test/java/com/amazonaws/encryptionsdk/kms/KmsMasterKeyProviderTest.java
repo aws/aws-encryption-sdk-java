9c8a81d0727152dd8ebc144a0e274b225352f922
Valerie Lambert <lavaleri@amazon.com>
2021-12-09 15:59:13 -0800

    format

--- src/test/java/com/amazonaws/encryptionsdk/kms/KmsMasterKeyProviderTest.java
+++ src/test/java/com/amazonaws/encryptionsdk/kms/KmsMasterKeyProviderTest.java
@@ -18,8 +18,8 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import com.amazonaws.AmazonServiceException;
